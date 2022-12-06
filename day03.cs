480ee605d6edc4d30fe6ea65a4037199b87569ec
TerryGamon <69677871+TerryGamon@users.noreply.github.com>
2022-12-06 09:21:23 +0100

    Add files via upload

---
+++ day03.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventofcode.Tag._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var input = File.ReadAllText("../../input.txt").Split("\n".ToCharArray());
+            var sum = 0;
+            
+            foreach (var line in input)
+            {
+                var left = line.Substring(0, line.Length / 2);
+                var right = line.Substring(line.Length / 2, line.Length / 2);
+
+                var query = (from ch in left where right.Contains(ch) select ch).Distinct();
+                foreach (var c in query)
+                    sum += char.IsLower(c) ? c - 96 : c - 38;
+            }
+
+            Console.WriteLine(sum); //7581
+
+            var i = 0;
+            sum = 0;
+            while (true)
+            {
+                var left = input.Skip(i).Take(3).ToArray();
+                if (left.Count() < 3)
+                    break;
+
+                var query = (from ch in left[0] where left[1].Contains(ch) && left[2].Contains(ch) select ch).Distinct();
+                foreach (var c in query)
+                    sum += char.IsLower(c) ? c - 96 : c - 38;
+
+                var right = input.Skip(i+3).Take(3).ToArray();
+                query = (from ch in right[0] where right[1].Contains(ch) && right[2].Contains(ch) select ch).Distinct();
+                foreach (var c in query)
+                    sum += char.IsLower(c) ? c - 96 : c - 38;
+
+                i += 6;
+            }
+
+            Console.WriteLine(sum); //2525
+        }
+    }
+}
---
+++ day04.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Adventofcode.Tag._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("../../input.txt").Split("\n".ToCharArray());
+            var count1 = 0;
+            var count2 = 0;
+            foreach (var line in input)
+            {
+                if (string.IsNullOrEmpty(line))
+                    break;
+                var left = GetNumbers(line.Split(',')[0]);
+                var right = GetNumbers(line.Split(',')[1]);
+
+                if (left.Intersect(right).ToList().Count == right.Count || right.Intersect(left).ToList().Count == left.Count)
+                    count1++;
+                if (left.Intersect(right).ToList().Count > 0 || right.Intersect(left).ToList().Count > 0)
+                    count2++;
+            }
+
+            Console.WriteLine(count1); //562
+            Console.WriteLine(count2); //924
+        }
+
+        static List<int> GetNumbers(string s)
+        {
+            var list = new List<int>();
+            var values = s.Split('-');
+            for (var i = Convert.ToInt32(values[0]); i <= Convert.ToInt32(values[1]); i++)
+                list.Add(i);
+            return list;
+        }
+    }
+}
